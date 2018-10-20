2b916cfb5b1aafd2596c6aa502d3c9e05f5a8a8c
djowroots <43259672+djowroots@users.noreply.github.com>
2018-10-20 05:20:51 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config                            = {}
 Config.DrawDistance               = 100.0
 Config.MarkerType                 = 1
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
-Config.MarkerColor                = { r = 50, g = 50, b = 204 }
+Config.MarkerColor                = { r = 200, g = 0, b = 200 }
 Config.EnablePlayerManagement     = true
 Config.EnableArmoryManagement     = true
 Config.EnableESXIdentity          = true -- only turn this on if you are using esx_identity
@@ -10,55 +10,49 @@ Config.EnableNonFreemodePeds      = false -- turn this on if you want custom ped
 Config.EnableSocietyOwnedVehicles = false
 Config.EnableLicenses             = true
 Config.MaxInService               = -1
-Config.Locale                     = 'sv'
+Config.Locale                     = 'en'
 
 Config.MafiaStations = {
 
   Mafia = {
 
     Blip = {
-     -- Pos     = { x = 425.130, y = -979.558, z = 30.711 },
-      Sprite  = 60,
+      Pos     = { x = 98.130, y = -1947.558, z = 20.711 },
+      Sprite  = 378,
       Display = 4,
-      Scale   = 1.2,
-      Colour  = 29,
+      Scale   = 1.1,
+      Colour  = 1,
     },
 
     AuthorizedWeapons = {
-      { name = 'WEAPON_COMBATPISTOL',     price = 30000 },
-      { name = 'WEAPON_ASSAULTSMG',       price = 1125000 },
-      { name = 'WEAPON_ASSAULTRIFLE',     price = 1500000 },
-      { name = 'WEAPON_PUMPSHOTGUN',      price = 600000 },
-      { name = 'WEAPON_STUNGUN',          price = 50000 },
-      { name = 'WEAPON_FLASHLIGHT',       price = 800 },
-      { name = 'WEAPON_SNIPERRIFLE',      price = 2200000 },
-      { name = 'WEAPON_APPISTOL',         price = 70000 },
-      { name = 'WEAPON_CARBINERIFLE',     price = 1100000 },
+      { name = 'WEAPON_ASSAULTRIFLE',     price = 70000 },
+      { name = 'WEAPON_PUMPSHOTGUN',      price = 9000 },
+      { name = 'WEAPON_FLASHLIGHT',       price = 80 },
+      { name = 'WEAPON_SNIPERRIFLE',      price = 80000 },
+      { name = 'WEAPON_SNSPISTOL',      price = 2000 },
+      { name = 'WEAPON_MACHINEPISTOL',      price = 70000 },
     },
 
 	  AuthorizedVehicles = {
-		  { name = 'schafter3',  label = 'Civil Vehicle' },
-		  { name = 'sandking',   label = 'Sandking 4X4' },
-		  { name = 'mule3',      label = 'Transport truck' },
-		  { name = 'guardian',   label = 'Guardian 4x4' },
-		  { name = 'rumpo3',     label = 'Rumpo' },
-		  { name = 'mesa',       label = 'mesa' },
+		  { name = 'chino2',  label = 'Chino 2 Gangster' },
+		  { name = 'virgo2',   label = 'Virgo 2 Gangster' },
+		  { name = 'sabregt3',      label = 'SabreGt Gagster' },
+		  { name = 'pony2',   label = 'Van da Verdinha' },
 	  },
 
     Cloakrooms = {
-      { x = 9.283, y = 528.914, z = 169.635 },
+      { x = 85.01, y = -1958.5, z = 19.70 },
     },
 
     Armories = {
-      { x = 1.550, y = 527.397, z = 169.617 },
-	  { x = -807.72, y = 167.32, z = 76 },
+	  { x = 85.16, y = -1954.35, z = 19.5 },
     },
 
     Vehicles = {
       {
-        Spawner    = { x = 13.40, y = 549.1, z = 175.187 },
-        SpawnPoint = { x = 8.237, y = 556.963, z = 175.266 },
-        Heading    = 90.0,
+        Spawner    = { x = 87.17, y = -1951.2, z = 19.4 },
+        SpawnPoint = { x = 87.63, y = -1968.70, z = 19.4 },
+        Heading    = 318.0,
       }
     },
 
@@ -71,12 +65,11 @@ Config.MafiaStations = {
     },
 
     VehicleDeleters = {
-      { x = 22.74, y = 545.9, z = 175.027 },
-      { x = 21.35, y = 543.3, z = 175.027 },
+      { x = 85.16, y = -1971.18, z = 19.40 },
     },
 
     BossActions = {
-      { x = 4.113, y = 526.897, z = 173.628 }
+      { x = 88.70, y = -1956.1, z = 19.40 }
     },
 
   },
